71b813769354e99f494a4e1c3e7a34495f5f50f5
Erick Kasih <erick.kasih@gmail.com>
2020-09-03 17:48:52 +0700

    new

--- FinAccel/ViewController.swift
+++ FinAccel/ViewController.swift
@@ -8,13 +8,195 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIScrollViewDelegate, UITableViewDelegate, UITableViewDataSource {
+    
 
+    @IBOutlet weak var topTabbedView: UIView!
+    @IBOutlet weak var mobileImageView: UIImageView!
+    @IBOutlet weak var mobileTF: UITextField!
+    @IBOutlet weak var viewForSVs: UIView!
+    @IBOutlet weak var promosLabelV: UIView!
+    
+    var scrollView1, scrollView2 : UIScrollView!
+    var tableView1, tableView2 : UITableView!
+    var tabButtons : [tabbedButton] = []
+    
+    let fSW = UIScreen.main.bounds.width
+    
+    let colors = [UIColor.purple, UIColor.systemBlue, UIColor.systemGray]
+    let pulsas = ["25.000", "50.000", "100.000", "150.000", "200.000", "250.000"]
+    let packetDatas = [["5 GB", "25.000"], ["10 GB", "50.000"], ["20 GB", "100.000"], ["30 GB", "150.000"], ["40 GB", "200.000"], ["50 GB", "250.000"]]
+    
+    var promosObjStart : CGFloat = 0
+    var promosItemWidth : CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .white
+        
+        let tabButton1 = tabbedButton(frame: CGRect(x: 0, y: 0, width: fSW/2, height: topTabbedView.frame.height))
+        topTabbedView.addSubview(tabButton1)
+        
+        tabButton1.setTitle("Pulsa", for: .normal)
+        tabButton1.setTitleColor(.black, for: .normal)
+        tabButton1.addTarget(self, action: #selector(changeTableView(sender:)), for: .touchUpInside)
+        
+        let tabButton2 = tabbedButton(frame: CGRect(x: fSW/2, y: 0, width: fSW/2, height: topTabbedView.frame.height))
+        topTabbedView.addSubview(tabButton2)
+        
+        tabButton2.setTitle("Packet Data", for: .normal)
+        tabButton2.setTitleColor(.black, for: .normal)
+        tabButton2.addTarget(self, action: #selector(changeTableView(sender:)), for: .touchUpInside)
+        
+        tabButtons.append(tabButton1)
+        tabButtons.append(tabButton2)
+        
+        mobileImageView.layer.borderWidth = 2.0
+        mobileImageView.layer.borderColor = UIColor.systemBlue.cgColor
+        mobileImageView.layer.masksToBounds = true
+        mobileImageView.layer.cornerRadius = mobileImageView.frame.height/2
+        
+        mobileTF.delegate = self
+        
+        scrollView2 = UIScrollView(frame: CGRect(x: 0, y: viewForSVs.frame.height - (fSW * 0.5), width: fSW, height: fSW * 0.5))
+        viewForSVs.addSubview(scrollView2)
+        
+        scrollView2.showsHorizontalScrollIndicator = false
+        scrollView2.delegate = self
+        
+        ///promosLabelV is without contraints in storyboard, that is why we can change its position
+        promosLabelV.frame = CGRect(x: 0, y: scrollView2.frame.origin.y - 50, width: fSW, height: 50)
+        
+        scrollView1 = UIScrollView(frame: CGRect(x: 0, y: 0, width: fSW, height: viewForSVs.frame.height - promosLabelV.frame.origin.y))
+        viewForSVs.addSubview(scrollView1)
+        
+        ///Scrollview 1 fillings containing : 2 table views
+        tableView1 = UITableView(frame: CGRect(x: 0, y: 0, width: fSW, height: scrollView1.frame.height))
+        scrollView1.addSubview(tableView1)
+        
+        tableView2 = UITableView(frame: CGRect(x: fSW, y: 0, width: fSW, height: scrollView1.frame.height))
+        scrollView1.addSubview(tableView2)
+        
+        ///Scrollview 2 fillings using : array from 'colors[]'
+        let objWidth = fSW * 0.8
+        let objHeight = objWidth/2
+        let objY = (scrollView2.frame.height - objHeight)/2
+        let objSpacing : CGFloat = 20
+        
+        var objX = objSpacing
+        
+        for i in 0..<colors.count {
+            
+            let colorB = UIButton(frame: CGRect(x: objX, y: objY, width: objWidth, height: objHeight))
+            scrollView2.addSubview(colorB)
+            
+            colorB.backgroundColor = colors[i]
+            colorB.layer.masksToBounds = true
+            colorB.layer.cornerRadius = 20.0
+            colorB.addTarget(self, action: #selector(toPromoPage), for: .touchUpInside)
+            
+            objX += objWidth + objSpacing
+            
+        }
+        
+        /// promosObjStart & promosItemWidth for scrollview snap effect usage
+        promosObjStart = objSpacing/2
+        promosItemWidth = objWidth + objSpacing
+        
+        scrollView2.contentSize = CGSize(width: objX, height: scrollView2.frame.height)
+        
     }
+    
+    @objc func changeTableView(sender: tabbedButton) {
+        
+        for button in tabButtons {
+            button.lineView.isHidden = true
+        }
+        
+        sender.lineView.isHidden = false
+        
+        if sender.titleLabel?.text!.lowercased() == "pulsa" {
+            scrollView1.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+        }else{
+            scrollView1.setContentOffset(CGPoint(x: fSW, y: 0), animated: true)
+        }
+        
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+        if scrollView == scrollView2 {
+            print(scrollView.contentOffset.x)
+            print(scrollView.contentSize.width)
+            
+            if scrollView.contentOffset.x < promosObjStart + promosItemWidth/2 {
+                scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+            }else if scrollView.contentOffset.x < promosObjStart + promosItemWidth + promosItemWidth/2 {
+                scrollView.setContentOffset(CGPoint(x: promosItemWidth, y: 0), animated: true)
+            }else{
+                scrollView.setContentOffset(CGPoint(x: scrollView.contentSize.width - fSW, y: 0), animated: true)
+            }
+            
+        }
+        
+        
+        
+    }
+    
+    @objc func toPromoPage(sender: UIButton) {
+        print("promo \(sender.backgroundColor)")
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        print(textField.text?.count)
+        
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView == tableView1 {
+            return pulsas.count
+        }else {
+            return packetDatas.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+}
 
+class tableCell : UITableViewCell {
+    
+}
 
+class tabbedButton : UIButton {
+    var lineView : UIView!
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        lineView = UIView(frame: CGRect(x: 0, y: self.frame.height - 3, width: self.frame.width, height: 3))
+        self.addSubview(lineView)
+        
+        lineView.backgroundColor = UIColor.systemRed
+        
+    }
+    
 }
 
